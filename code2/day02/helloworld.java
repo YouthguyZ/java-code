faa8b535caa21690df0244f2fee317da08b469de
youngguyz <2281300996@qq.com>
2022-11-30 12:36:49 +0800

    feat: day02

---
+++ code2/day02/helloworld.java
@@ -0,0 +1,11 @@
+public class helloworld {
+    public static void main(String[] args) {
+        int i = 60;
+        System.out.println(i);
+        System.out.println(666666);
+        // 注释
+        // Scanner
+        String a = "生活";
+        System.out.println(a);
+    }
+}
